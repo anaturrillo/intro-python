14e539d15a6dd947a8cc9c733761639571ad11f7
ana turrillo <ana.turrillo@wolox.com.ar>
2019-11-01 17:33:40 -0300

    ejercicios 1 a 3bis

---
+++ clase_01/ejercicios/ej_03_bis.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 3) Modificar la función anterior una vez más para que además el usuario pueda ingresar una operación y retornar el resultado de la operación (+, -, *, /)
+# Solución alternativa
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b        
+
+operaciones = {
+    "+": sumar,
+    "-": restar,
+    "*": multiplicar,
+    "/": dividir
+}
+
+def operar():
+    num_1 = input('Ingrese el primer número: ')
+    operacion = input('Ingrese la operación: ')
+    num_2 = input('Ingrese el siguiente número: ')
+    
+    num_1_int = int(num_1)
+    num_2_int = int(num_2)
+    
+    return operaciones[operacion](num_1_int, num_2_int)
+
+print(operar())
