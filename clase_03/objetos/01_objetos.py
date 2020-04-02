6658ba118f0e02acd72920f9db9ee837b4ac9b72
tferrerITBA <tferrer@itba.edu.ar>
2020-04-02 11:20:25 -0300

    Cambio en propiedad color de Gato

--- clase_03/objetos/01_objetos.py
+++ clase_03/objetos/01_objetos.py
@@ -1,11 +1,10 @@
 class Gato:
-    color = 'gris'
+    def __init__(self, color):
+        self.color = color
 
     def caminar(self):
         print('estoy caminando')
 
-
-gato_gris = Gato()
-
+gato_gris = Gato('gris')
 gato_gris.caminar()
 print(gato_gris.color)
